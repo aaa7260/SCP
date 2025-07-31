2dbe33dbc356b82aa58163e1ac228604d68bc13c
aaa7260 <62923926+aaa7260@users.noreply.github.com>
2025-07-31 20:52:17 +0800

    Update SCP-workflow.R
    Signed-off-by: aaa7260 <62923926+aaa7260@users.noreply.github.com>

--- R/SCP-workflow.R
+++ R/SCP-workflow.R
@@ -1775,11 +1775,11 @@ scVI_integrate <- function(srtMerge = NULL, batch = NULL, append = TRUE, srtList
       srtIntegrated <- FindClusters(object = srtIntegrated, resolution = cluster_resolution, algorithm = cluster_algorithm_index, method = "igraph", graph.name = "scVI_SNN", verbose = FALSE)
       cat(paste0("[", Sys.time(), "]", " Reorder clusters...\n"))
       #if (inherits(srtIntegrated[["scVIcorrected"]], "Assay5")) {
-      DefaultLayer(srtIntegrated[["scVIcorrected"]]) <- "counts"
-      srtIntegrated <- SrtReorder(srtIntegrated, features = HVF, reorder_by = "seurat_clusters", slot = "counts")  
+      #DefaultLayer(srtIntegrated[["scVIcorrected"]]) <- "counts"
+      #srtIntegrated <- SrtReorder(srtIntegrated, features = HVF, reorder_by = "seurat_clusters", slot = "counts")  
       # scvi-tools官方建议用counts，本counts为scvi生成的normalized，srtIntegrated未NormalizedData，srtIntegrated内无data层
       #} else {
-      #  srtIntegrated <- SrtReorder(srtIntegrated, features = HVF, reorder_by = "seurat_clusters", slot = "data") # 比如默认是 "data"
+      srtIntegrated <- SrtReorder(srtIntegrated, features = HVF, reorder_by = "seurat_clusters", slot = "data") # 比如默认是 "data"
       #}
       srtIntegrated[["seurat_clusters"]] <- NULL
       srtIntegrated[["scVIclusters"]] <- Idents(srtIntegrated)
